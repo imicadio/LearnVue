f3f8601a587033282b67d91bd9487ef1d1a61751
Michał <mrjesio@gmail.com>
2020-01-02 23:47:42 +0100

    :style player, monster life

---
+++ MonsterSlayer/app.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: "#app",
+    // przechowywanie danych, zmiennych
+    data: {     
+        playerHealth: 100,
+        monsterHealth: 100,
+        gameIsRuning: false,
+        
+    }
+});
