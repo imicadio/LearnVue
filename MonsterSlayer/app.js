63c8378bf46529b71d033a42f329f71a041e579d
Michał <mrjesio@gmail.com>
2020-01-03 12:54:47 +0100

    attack method

--- MonsterSlayer/app.js
+++ MonsterSlayer/app.js
@@ -11,6 +11,38 @@ new Vue({
             this.gameIsRuning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack: function() {
+            var max = 10;
+            var min = 3;
+            var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+            this.monsterHealth -= damage;
+
+            if(this.monsterHealth <= 0) {
+                alert('You won!');
+                this.gameIsRuning = false;
+                return;
+            }
+
+            var max = 12;
+            var min = 5;
+            var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+            this.playerHealth -= damage;
+
+            if(this.playerHealth <= 0) {
+                alert('You lost!');
+                this.gameIsRuning = false;
+                return;
+            }
+        },
+        specialAttack: function() {
+
+        },
+        heal: function() {
+
+        },
+        giveUp: function() {
+
         }
     }
 });
